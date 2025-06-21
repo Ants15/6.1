cfffef612b705cdd0a7dc2c12415f041fe100a0d
Ants15 <antmerce@uat.edu>
2025-06-21 17:31:55 -0600

    Add files via upload

---
+++ 6.1/strings.js
@@ -0,0 +1,26 @@
+function validateInputs(){
+    let first = document.getElementById("firstName").value.trim();
+    let last= document.getElementById("lastName").value.trim();
+    let zip=document.getElementById("zip").value.trim();
+    let fullName= first + " " + last;
+    let output = document.getElementById("output");
+    let img= document.getElementById("secretimg");
+    output.innerHTML= "";
+    img.style.display= "none";
+    if(fullName > 20){
+        output.innerHTML= "Your full name is too long (moore than 20 characters!).";
+        return;
+    }
+    if(zip.length!==  5 || isNaN(zip)){
+        output.innerHTML= "Zip code must be exactly 5 numeric digits.";
+        return;
+    }
+    output.innerHTML =`<h2>Access Granted!</h2><p>Welcome, ${fullName} from ${zip}.<br>Here is your secret message:</p><p><strong> "This is my dog Rufus and he likes glasses! For more fun scroll to bottom! _/¯(ツ)_/¯" </strong></p>`;
+    img.style.display= "inline"; 
+}
+// I know we were supposed to use this "actionlistener" but I added this for a fun little detail
+function displayimage(){
+    let img= document.getElementById("sprscrt")
+    img.style.display = "inline";
+    document.getElementById("supersecret").addEventListener("click", displayimage)
+}
